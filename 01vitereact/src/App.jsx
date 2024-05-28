d5fd1602f1f4bfe5830bb7eaa3149c755b9480e6
Aditya Kumar <theadityakumar2810@gmail.com>
2024-05-29 00:13:57 +0530

    first commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./Chai"
+
+function App() {
+
+  return (
+    // fragment => return only one value
+    <> 
+      <Chai/>
+      <h3> Apurv is Chilkut</h3>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Chai = () => {
+  return (
+    <h1>Laddu Singh || Motu || Tiger</h1>
+  )
+}
+
+export default Chai
