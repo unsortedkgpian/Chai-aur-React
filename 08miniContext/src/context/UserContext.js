ef1c720a3f54b3664480427aa8c8f588b6f81b61
Aditya Kumar <theadityakumar2810@gmail.com>
2024-06-02 13:46:55 +0530

    08 Project

---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const UserContext = React.createContext();
+
+export default UserContext;
+
+
+// <UserContext>
+//     <App>
+//         <Card>
+//             <Data />
+//         </Card>
+//     </App>
+// < / UserContext />
